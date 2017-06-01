08c0ac1b610c28d60c2e6583e4ff62604be99931
zekrom-vale <sgraven66@gmail.com>
2017-06-01 18:46:46 -0500

    Added play check

--- Extention/controll.js
+++ Extention/controll.js
@@ -1,20 +1,27 @@
 //Youtube is more pixalated
-var rgb=0;
-setInterval(evalu, 100);
+var rgb=0,
+clock= setInterval(evalu, 10000);
 
 function evalu(){
-	//Stop if the vedieo is paused
-	document.getElementsByClassName('html5-main-video')[0].style.filter='';
-	getAvColor(document.getElementsByClassName('html5-main-video')[0]);
-	rgb= 255- rgb;
-	console.log(rgb);
-	var bright= 0.0000159587 * Math.pow(rgb, 2) + 0.000691772 * rgb + 0.972228,
-	invert= 0,
-	con= 1.01 * rgb,
-	sat= 1.013 * rgb;
-	setFilter(bright, invert, con, sat);
-	rgb=0;
+	if(/playing-mode/i.test(document.getElementById('movie_player').class)){
+		//Stop if the vedieo is paused
+		document.getElementsByClassName('html5-main-video')[0].style.filter='';
+		getAvColor(document.getElementsByClassName('html5-main-video')[0]);
+		rgb= 255- rgb;
+		console.log(rgb);
+		var bright= 0.0000178544 * Math.pow(rgb, 2) - 0.0016272 * rgb + 1.01409,
+		invert= 0,
+		con= 1.01 * rgb,
+		sat= 1.013 * rgb;
+		setFilter(bright, invert, con, sat);
+		rgb=0;
+	}
 }
+
+
+class= html5-video-player iv-module-loaded ytp-hide-info-bar paused-mode
+class= html5-video-player iv-module-loaded ytp-hide-info-bar playing-mode ytp-autohide
+
 
 function setFilter(bright, invert, con, sat){
 	document.getElementsByClassName('html5-main-video')[0].style.filter='brightness('+ bright+ ')';//+' invert('+ invert+') contrast('+ con+ ') saturate('+ sat+ ')';
@@ -24,6 +31,7 @@ function getAvColor(img) {
     var canvas= document.createElement('canvas'),
         context= canvas.getContext && canvas.getContext('2d'),
         count= 0;
+	document.body.appendChild(canvas);
 	canvas.id= 'temp';
     // return the base colour for non-compliant browsers
     if(!context) return;
@@ -35,12 +43,13 @@ function getAvColor(img) {
     context.drawImage(img, 0, 0);
 
     try{
-        data= context.getImageData(0, 0, width, height);
+        data= context.getImageData(100, 100, width - 100, height - 100);
     } catch(e) {
         // catch errors - usually due to cross domain security issues
         console.log(e);
         return;
     }
+	document.body.removeChild(canvas);
 	var i= C= 0;
 	while(i< data.data.length){
 		rgb+= Number(data.data[i]);
