9cc1b4087e7f06b09631840a597d845d6d8bbc53
zekrom-vale <sgraven66@gmail.com>
2017-06-01 15:26:45 -0500

    Calibrated brightness

--- Extention/controll.js
+++ Extention/controll.js
@@ -1,31 +1,30 @@
-"strict mode";
+//Youtube is more pixalated
 var rgb=0;
-evalu();
 setInterval(evalu, 100);
 
 function evalu(){
+	//Stop if the vedieo is paused
+	document.getElementsByClassName('html5-main-video')[0].style.filter='';
 	getAvColor(document.getElementsByClassName('html5-main-video')[0]);
 	rgb= 255- rgb;
 	console.log(rgb);
-	rgb= rgb/255;
-	console.log(rgb);
-	var bright= 2 * rgb,
-	invert= 0.3 * rgb,
-	con= 1.3 * rgb,
-	sat= 1.27 * rgb;
+	var bright= 0.0000159587 * Math.pow(rgb, 2) + 0.000691772 * rgb + 0.972228,
+	invert= 0,
+	con= 1.01 * rgb,
+	sat= 1.013 * rgb;
 	setFilter(bright, invert, con, sat);
 	rgb=0;
 }
 
 function setFilter(bright, invert, con, sat){
-	document.getElementsByClassName('html5-main-video')[0].style.filter='brightness('+ bright+ ') invert('+ invert+') contrast('+ con+ ') saturate('+ sat+ ')';
+	document.getElementsByClassName('html5-main-video')[0].style.filter='brightness('+ bright+ ')';//+' invert('+ invert+') contrast('+ con+ ') saturate('+ sat+ ')';
 }
 
 function getAvColor(img) {
     var canvas= document.createElement('canvas'),
         context= canvas.getContext && canvas.getContext('2d'),
         count= 0;
-
+	canvas.id= 'temp';
     // return the base colour for non-compliant browsers
     if(!context) return;
 
@@ -39,13 +38,16 @@ function getAvColor(img) {
         data= context.getImageData(0, 0, width, height);
     } catch(e) {
         // catch errors - usually due to cross domain security issues
-        alert(e);
+        console.log(e);
         return;
     }
 	var i= C= 0;
 	while(i< data.data.length){
 		rgb+= Number(data.data[i]);
-		i+= 100;
+		var Ran= Math.random();
+		Ran= Ran - 0.5;
+		Ran= Math.floor(Ran * 400);
+		i+= 1000 + Ran;
 		C++;
 	}
     rgb= rgb/C;
