9e90552fa67a7160d9280f1cba44089041643654
zekrom-vale <sgraven66@gmail.com>
2017-06-01 19:18:11 -0500

    Fixed dectect play

--- Extention/controll.js
+++ Extention/controll.js
@@ -1,27 +1,23 @@
 //Youtube is more pixalated
 var rgb=0,
-clock= setInterval(evalu, 10000);
+clock= setInterval(evalu, 100);
 
 function evalu(){
-	if(/playing-mode/i.test(document.getElementById('movie_player').class)){
-		//Stop if the vedieo is paused
+	var el= document.getElementById('movie_player').classList;
+	el= el.toString();
+	if(el.includes('playing-mode')){
 		document.getElementsByClassName('html5-main-video')[0].style.filter='';
 		getAvColor(document.getElementsByClassName('html5-main-video')[0]);
 		rgb= 255- rgb;
 		console.log(rgb);
-		var bright= 0.0000178544 * Math.pow(rgb, 2) - 0.0016272 * rgb + 1.01409,
-		invert= 0,
-		con= 1.01 * rgb,
-		sat= 1.013 * rgb;
+		var bright= 0.0000178544* Math.pow(rgb, 2)- 0.0016272* rgb+ 1.01409,
+		invert= 1- rgb,
+		con= 1.01* rgb,
+		sat= 1.013* rgb;
 		setFilter(bright, invert, con, sat);
 		rgb=0;
 	}
 }
-
-
-class= html5-video-player iv-module-loaded ytp-hide-info-bar paused-mode
-class= html5-video-player iv-module-loaded ytp-hide-info-bar playing-mode ytp-autohide
-
 
 function setFilter(bright, invert, con, sat){
 	document.getElementsByClassName('html5-main-video')[0].style.filter='brightness('+ bright+ ')';//+' invert('+ invert+') contrast('+ con+ ') saturate('+ sat+ ')';
@@ -43,7 +39,7 @@ function getAvColor(img) {
     context.drawImage(img, 0, 0);
 
     try{
-        data= context.getImageData(100, 100, width - 100, height - 100);
+        data= context.getImageData(0, 0, width, height);
     } catch(e) {
         // catch errors - usually due to cross domain security issues
         console.log(e);
