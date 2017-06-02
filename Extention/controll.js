77c542f0453a98c6fa5e85ee3b56546c5da64089
zekrom-vale <sgraven66@gmail.com>
2017-06-01 20:36:30 -0500

    Now skips alfa
    The dakness is more aperent

--- Extention/controll.js
+++ Extention/controll.js
@@ -7,14 +7,14 @@ function evalu(){
 	el= el.toString();
 	if(el.includes('playing-mode')){
 		document.getElementsByClassName('html5-main-video')[0].style.filter='';
-		getAvColor(document.getElementsByClassName('html5-main-video')[0]);
+		getAvColor(document.getElementsByClassName('html5-main-video')[0]);//This is it
 		rgb= 255- rgb;
 		console.log(rgb);
-		var bright= 0.0000178544* Math.pow(rgb, 2)- 0.0016272* rgb+ 1.01409,
+		var bright= 0.0000075391* Math.pow(rgb, 2)+ 0.00185722* rgb+ 1.05812,
 		invert= 1- rgb,
 		con= 1.01* rgb,
 		sat= 1.013* rgb;
-		setFilter(bright, invert, con, sat);
+		setFilter(bright, invert, con, sat);//nope
 		rgb=0;
 	}
 }
@@ -36,9 +36,9 @@ function getAvColor(img) {
     var height= canvas.height= img.naturalHeight || img.offsetHeight || img.height,
         width= canvas.width= img.naturalWidth || img.offsetWidth || img.width;
 
-    context.drawImage(img, 0, 0);
+    context.drawImage(img, 0, 0);//Disable hardware acceleration
 
-    try{
+    try{//Nope
         data= context.getImageData(0, 0, width, height);
     } catch(e) {
         // catch errors - usually due to cross domain security issues
@@ -47,13 +47,20 @@ function getAvColor(img) {
     }
 	document.body.removeChild(canvas);
 	var i= C= 0;
-	while(i< data.data.length){
+	while(i< data.data.length){//Nope!
+		var Z= Math.random();
+		Z= Math.round(Z* 3) +1;
+		rgb+= Number(data.data[Z]);
+		i+= 12;
+		C++;
+	}
+	/*while(i< data.data.length){//Nope!
 		rgb+= Number(data.data[i]);
 		var Ran= Math.random();
 		Ran= Ran - 0.5;
 		Ran= Math.floor(Ran * 400);
 		i+= 1000 + Ran;
 		C++;
-	}
+	}*/
     rgb= rgb/C;
 }
