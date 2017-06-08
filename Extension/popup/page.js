1fb5665e97166c318243236c09ba5c999f568537
zekrom-vale <sgraven66@gmail.com>
2017-06-07 20:32:50 -0500

    Added icon swap!

--- Extension/popup/page.js
+++ Extension/popup/page.js
@@ -13,16 +13,35 @@ document.addEventListener('DOMContentLoaded', function() {
 function IO(){
 	var IOc= document.getElementById('IO').checked;
 	chrome.storage.local.set({'Active': IOc}, function(){console.log('saved!')})
-	/* 	if(IOc){
-		delay= Number(delay);
-		Initialize= setInterval(look, 1000);
-	}
-	else{
-		clearInterval(Initialize);
-		clearInterval(clock);
-	} */
+	Dlt();
 }
 function Srt(){
 	var STc= document.getElementById('ST').checked;
 	chrome.storage.local.set({'Short': STc}, function(){console.log('saved!')})
+	Dlt();
+}
+
+function Dlt(){
+	if(document.getElementById('ST').checked=== true || document.getElementById('IO').checked=== false) disabled();
+	else enabled();
+}
+
+function enabled(){
+	chrome.browserAction.setIcon({
+		path:{
+			"16": "../img/YT16.png",
+			"32": "../img/YT32.png",
+			"64": "../img/YT64.png",
+			"128": "../img/YT128.png"
+		}
+	});
+}
+function disabled(){
+	chrome.browserAction.setIcon({
+		path:{
+			"16": "../img/off16.png",
+			"32": "../img/off32.png",
+			"64": "../img/off64.png"
+		}
+	});
 }
