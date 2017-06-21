acd7dba433d33de48e7a3dd28ae3fbff8ec50b55
zekrom-vale <sgraven66@gmail.com>
2017-06-20 20:27:36 -0500

    Now saves normal CSS on reset

--- Extension/popup/opt.js
+++ Extension/popup/opt.js
@@ -7,31 +7,25 @@ document.addEventListener('DOMContentLoaded', (event)=>{
 		//Load new values
 		BROWSER.storage.local.get(['PozOn', 'PozSkip', 'PozCSS', 'Adv', 'AdvOn'], items=>{
 			document.getElementById('OnOff').value= items.PozOn;
+			//Set normal CSS
 			document.getElementById('Apply').value= items.PozSkip=== true? 'ovrd,#Brt-opt': items.PozCSS.apply;
 			document.getElementById('position').value= items.PozCSS.position;
-			
 			document.getElementById('TB').value= items.PozCSS.TB[0];		
 			document.getElementById('TBn').value= items.PozCSS.TB[2];		
 			document.getElementById('TBu').value= items.PozCSS.TB[3];
-			
 			document.getElementById('RL').value= items.PozCSS.RL[0];		
 			document.getElementById('RLn').value= items.PozCSS.RL[2];		
 			document.getElementById('RLu').value= items.PozCSS.RL[3];
-			
 			document.getElementById('PTn').value= items.PozCSS.PT[0];
 			document.getElementById('PTu').value= items.PozCSS.PT[1];
-			
 			document.getElementById('PRn').value= items.PozCSS.PR[0];
 			document.getElementById('PRu').value= items.PozCSS.PR[1];
-			
 			document.getElementById('PBn').value= items.PozCSS.PB[0];
 			document.getElementById('PBu').value= items.PozCSS.PB[1];
-			
 			document.getElementById('PLn').value= items.PozCSS.PL[0];
 			document.getElementById('PLu').value= items.PozCSS.PL[1];
-			
 			document.getElementById('Bc').value= items.PozCSS.Bc;
-			
+			//Set Advanced CSS
 			items.Adv=== undefined? items.Adv= undefined :document.getElementById('adv').value= items.Adv;
 			document.getElementById('AdvOn').checked= items.Adv;
 		});
@@ -43,48 +37,7 @@ document.addEventListener('DOMContentLoaded', (event)=>{
 		BROWSER.storage.local.set({'PozOn':this.checked});
 	});
 	
-	document.getElementById('formCSS').addEventListener('change', ()=>{
-		//Save CSS
-		var ovrd= /ovrd/.test(document.getElementById('Apply').value),
-		apply= ovrd=== false? document.getElementById('Apply').value: '#Brt-opt';
-		BROWSER.storage.local.set({'PozCSS': //Sync??
-			{
-				'apply': apply,
-				'position': document.getElementById('position').value,
-				'TB': [
-					document.getElementById('TB').value,
-					':',
-					document.getElementById('TBn').value,
-					document.getElementById('TBu').value
-				],
-				'RL':[
-					document.getElementById('RL').value,
-					':',
-					document.getElementById('RLn').value,
-					document.getElementById('RLu').value
-				],
-				'PT':[
-					document.getElementById('PTn').value,
-					document.getElementById('PTu').value
-				],
-				'PR':[
-					document.getElementById('PRn').value,
-					document.getElementById('PRu').value
-				],
-				'PB':[
-					document.getElementById('PBn').value,
-					document.getElementById('PBu').value
-				],
-				'PL':[
-					document.getElementById('PLn').value,
-					document.getElementById('PLu').value
-				],
-				'Bc': document.getElementById('Bc').value
-			}
-		});
-		BROWSER.storage.local.set({'PozSkip': ovrd});
-		//End Save CSS
-	});
+	document.getElementById('formCSS').addEventListener('change', saveCSS);
 	//Save advanced CSS
 	document.getElementById('adv').addEventListener('change', ()=>{
 		BROWSER.storage.local.set({'Adv': document.getElementById('adv').value});
@@ -92,6 +45,10 @@ document.addEventListener('DOMContentLoaded', (event)=>{
 	document.getElementById('adv').addEventListener('keypress', ()=>{
 		BROWSER.storage.local.set({'Adv': document.getElementById('adv').value});
 	});
+	document.getElementById('AdvOn').addEventListener('change', function(){
+		BROWSER.storage.local.set({'AdvOn':this.checked});
+	});
+	//Reset Key
 	document.getElementById('arm').addEventListener('change', function(){
 		document.getElementById('reset').disabled=this.checked=== true? false: true;
 	});
@@ -105,7 +62,50 @@ document.addEventListener('DOMContentLoaded', (event)=>{
 			this.value= 'Reset';
 		}, 1000);
 	});
-	document.getElementById('AdvOn').addEventListener('change', function(){
-		BROWSER.storage.local.set({'AdvOn':this.checked});
+	document.getElementById('Nreset').addEventListener('click', function(){
+		document.getElementById('formCSS').reset();
+		saveCSS();
 	});
 });
+
+//Save Normal CSS
+function saveCSS(){
+	var ovrd= /ovrd/.test(document.getElementById('Apply').value),
+	apply= ovrd=== false? document.getElementById('Apply').value: '#Brt-opt';
+	BROWSER.storage.local.set({'PozCSS': //Sync??
+		{
+			'apply': apply,
+			'position': document.getElementById('position').value,
+			'TB': [
+				document.getElementById('TB').value,
+				':',
+				document.getElementById('TBn').value,
+				document.getElementById('TBu').value
+			],
+			'RL':[
+				document.getElementById('RL').value,
+				':',
+				document.getElementById('RLn').value,
+				document.getElementById('RLu').value
+			],
+			'PT':[
+				document.getElementById('PTn').value,
+				document.getElementById('PTu').value
+			],
+			'PR':[
+				document.getElementById('PRn').value,
+				document.getElementById('PRu').value
+			],
+			'PB':[
+				document.getElementById('PBn').value,
+				document.getElementById('PBu').value
+			],
+			'PL':[
+				document.getElementById('PLn').value,
+				document.getElementById('PLu').value
+			],
+			'Bc': document.getElementById('Bc').value
+		}
+	});
+	BROWSER.storage.local.set({'PozSkip': ovrd});
+}
