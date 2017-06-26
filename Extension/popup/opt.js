df974fd6bf0db3d803ebbba436d43b60e4fa58ac
zekrom-vale <sgraven66@gmail.com>
2017-06-26 16:17:32 -0500

    Added with for code compresion

--- Extension/popup/opt.js
+++ Extension/popup/opt.js
@@ -6,22 +6,24 @@ document.addEventListener('DOMContentLoaded', (event)=>{
 		document.getElementById('OnOff').checked= items.PozOn;
 		//Set normal CSS
 		document.getElementById('Apply').value= items.PozSkip=== true? 'ovrd,#Brt-opt': items.PozCSS.apply;
-		document.getElementById('position').value= items.PozCSS.position;
-		document.getElementById('TB').value= items.PozCSS.TB[0];		
-		document.getElementById('TBn').value= items.PozCSS.TB[2];		
-		document.getElementById('TBu').value= items.PozCSS.TB[3];
-		document.getElementById('RL').value= items.PozCSS.RL[0];		
-		document.getElementById('RLn').value= items.PozCSS.RL[2];		
-		document.getElementById('RLu').value= items.PozCSS.RL[3];
-		document.getElementById('PTn').value= items.PozCSS.PT[0];
-		document.getElementById('PTu').value= items.PozCSS.PT[1];
-		document.getElementById('PRn').value= items.PozCSS.PR[0];
-		document.getElementById('PRu').value= items.PozCSS.PR[1];
-		document.getElementById('PBn').value= items.PozCSS.PB[0];
-		document.getElementById('PBu').value= items.PozCSS.PB[1];
-		document.getElementById('PLn').value= items.PozCSS.PL[0];
-		document.getElementById('PLu').value= items.PozCSS.PL[1];
-		document.getElementById('Bc').value= items.PozCSS.Bc;
+		with(items.PozCSS){//with test
+			document.getElementById('position').value= position;
+			document.getElementById('TB').value=  TB[0];		
+			document.getElementById('TBn').value= TB[2];		
+			document.getElementById('TBu').value= TB[3];
+			document.getElementById('RL').value=  RL[0];		
+			document.getElementById('RLn').value= RL[2];		
+			document.getElementById('RLu').value= RL[3];
+			document.getElementById('PTn').value= PT[0];
+			document.getElementById('PTu').value= PT[1];
+			document.getElementById('PRn').value= PR[0];
+			document.getElementById('PRu').value= PR[1];
+			document.getElementById('PBn').value= PB[0];
+			document.getElementById('PBu').value= PB[1];
+			document.getElementById('PLn').value= PL[0];
+			document.getElementById('PLu').value= PL[1];
+			document.getElementById('Bc').value=  Bc;
+		}
 		//Set Advanced CSS
 		items.Adv=== undefined? items.Adv= undefined :document.getElementById('adv').value= items.Adv;
 		document.getElementById('AdvOn').checked= items.AdvOn;
