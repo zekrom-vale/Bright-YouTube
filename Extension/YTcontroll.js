0eeebc10c7a8ec7d31891e626c9f69c7c18cb024
zekrom-vale <sgraven66@gmail.com>
2017-09-09 10:49:11 -0500

    Compresses files and ads lang object
    The lang object will make using different languages eayseyer.

--- Extension/YTcontroll.js
+++ Extension/YTcontroll.js
@@ -30,10 +30,9 @@ function getFN(items){
 			}
 			else{
 				items.fn= undefined;
-				var warn= '[BREACH DETECTED]Custom function is invalid, bypased first checheck';
-				chrome.storage.local.set({'Err': {'time':Date(), 'code':407, 'text':warn}});
+				chrome.storage.local.set({'Err': {'time':Date(), 'code':407, 'text':lang.warn.breach}});
 				alert(warn+ ':\n'+ rez);
-				throw new SyntaxError(`%c${warn}:\n`+ rez, 'font-weight:bold');
+				throw new SyntaxError(`%c${lang.warn.breach}:\n`+ rez, 'font-weight:bold');
 			}
 		}
 	}catch(e){
@@ -46,12 +45,12 @@ function Intlize(items){
 	switch(typeof items.Active){
 		case 'undefined':
 		case 'null':
-			console.warn('Active is not defined, forced to true');
+			console.warn(lang.und.Active);
 			chrome.storage.local.set({
 				'Err':{
 					'time':Date(),
 					'code':404,
-					'text':'Active und, overiden'
+					'text':lang.und.Active
 				},
 				'Active': true
 			});
@@ -82,7 +81,7 @@ function Int2(items){
 	//Add option NOT to auto start
 	chrome.storage.local.get(['Active', 'Auto'], items=>{
 		if(items.Active!==false && items.Auto!==false){
-			console.log('Starting');
+			console.log(lang.stat.starting);
 			START();
 		}
 	});
@@ -147,156 +146,9 @@ ${apply}{
 					}
 				}
 			}
-			else console.info('[OFF] Inline IO, User specification');
+			else console.info(lang.stat.inIO);
 		});
 	}
-}
-//Active?
-function onPlay(){
-	clearInterval(clock);
-	clock= setInterval(evalu, DLY);
-	toggle();
-}
-
-function onPause(){
-	clearInterval(clock);//Sometimes fails
-	toggle(false);
-}
-
-function StorageChange(changes){
-	try{
-		with(changes.Active){
-			if(newValue== true) START();
-			else if(newValue== false) STOP();
-			else if(newValue== 'Short'){
-				console.info('[STOP] User specification');
-				SHORT();
-			}
-		}
-	}catch(e){}
-}
-//End Active?
-//STP
-function SHORT(){
-	console.log('SHORT');
-	onPause();
-	with(document.getElementsByTagName('video')[0]){
-		style.willChange= 'auto';
-		removeEventListener('play', onPlay);
-		removeEventListener('pause', onPause);
-		removeChild(document.getElementById('Brt-canvas'));
-		removeChild(document.getElementById('Brt-YT'));
-		style.willChange= '';
-	}
-	chrome.storage.onChanged.removeListener(StorageChange);
-}
-function STOP(){
-	console.log('STOP');
-	onPause();
-	with(document.getElementsByTagName('video')[0]){
-		style.willChange= 'auto';
-		removeEventListener('play', onPlay);
-		removeEventListener('pause', onPause);
-	}
-	document.getElementById('Brt-YT').innerHTML= '';
-}
-function START(){
-	console.log('SHORT');
-	document.getElementsByTagName('video')[0].style.willChange= 'filter';
-	clearInterval(clock);
-	clock= setInterval(evalu, DLY);
-	toggle();
-	with(document.getElementsByTagName('video')[0]){
-		addEventListener('play', onPlay);
-		addEventListener('pause', onPause);
-	}
-}
-//End STP
-function evalu(){
-	if(document.getElementsByClassName('audio_only_div')[0]){
-		SHORT();
-		console.info('[STOP] audio_only detected');
-		return;
-	}
-	if(document.getElementsByTagName('video')[0].style.filter!='' || document.getElementsByTagName('video')[0].readyState< 4) return;
-	if(document.webkitHidden || document.hidden){
-		//*
-			clearInterval(clock);
-			setTimeout(()=>{
-				clock= setInterval(evalu, DLY);
-			}, 5000);
-		//*/
-		return;
-	}
-	//security
-	if(isNaN(oldRgb)){
-		console.warn('Varables ilegaly modifyed');
-		clearInterval(clock);
-		oldRgb= 140;
-		clock= setInterval(evalu, DLY);
-		chrome.storage.local.set({
-			'Err':{
-				'time':Date(),
-				'code':100.7,
-				'text':'oldRgb isNaN->140'
-			}
-		});
-	}
-	document.getElementById('Brt-YT').innerHTML= '';
-	var av= getAvColor(),
-	U= (av.r+av.g+av.b)/3,
-	W= 0.2126*av.r+ 0.7152*av.g+ 0.0722*av.b,
-	rgb= 255-(.9*U+.1*W);
-	/*
-	with(console){
-		groupCollapsed('%crgb: %.2f', "color:blue",rgb);
-		info(av);
-		info('Unweighted rgb(U): %.5f', U);
-		info('Weighted rgb(W): %f.5', W);
-		info('Difference (U-W): %f.5', U-W);
-		info('rgb: %f.5', rgb);
-		timeStamp('time');
-		groupEnd();
-	}//*/
-	var IC= 1;
-	const inc= 1;
-	if(rgb< 254.9 && rgb> 20){
-		while(IC< 10){
-			let ic= IC;
-			setTimeout(CSfn(ic, rgb, U, W, r, g, b, oldRgb, oldW, oldU), (DLY*ic)/10);
-			IC+= inc;
-		}
-		oldRgb= rgb;
-		oldW= W;
-		oldU= U;
-	}
-	else if(rgb<= 20) setFilter(1, 1);
-}
-function setFilter(brt=1, vrt=0, con=1, sat=1){
-	brt= brt==1? '': `brightness(${brt}) `;
-	vrt= vrt==0? '': `invert(${vrt}) `;
-	con= con==1? '': `contrast(${con}) `;
-	sat= sat==1? '': `saturate(${sat})`;
-	if(brt+vrt+con+sat=='') document.getElementById('Brt-YT').innerHTML='';
-	else document.getElementById('Brt-YT').innerHTML= `video{\n\tfilter:${brt+ vrt+ con+ sat}\n}`;
-}
-
-function getAvColor(){
-	let VID=document.getElementsByTagName('video')[0];
-	var height= document.getElementById('Brt-canvas').height= VID.clientHeight,
-	width= document.getElementById('Brt-canvas').width= VID.clientWidth;
-	document.getElementById('Brt-canvas').getContext('2d', {willReadFrequently:true}).drawImage(VID, 0, 0, width, height,0,0,width,height);//hardware acceleration
-    data= document.getElementById('Brt-canvas').getContext('2d', {willReadFrequently:true}).getImageData(0,0, width, height);
-	var i= r= g= b= a= C= 0;
-	while(i< data.data.length){
-		r+= data.data[i];
-		g+= data.data[i+1];
-		b+= data.data[i+2];
-		//a+= data.data[i+3];
-		i+= Math.round(Math.random()*5 +1)*4;
-		C++;
-	}
-	return {'r':r/C, 'g':g/C, 'b':b/C, 'C':C};
 }
 //Indicate
 function toggle(poz= true){
@@ -304,37 +156,4 @@ function toggle(poz= true){
 	if(PLY!= false){
 		poz?PLY.classList.add('active'):PLY.classList.remove('active');
 	}
-}
-
-//reSet
-function reSet(){
-	var items={
-		'PozCSS': {},
-		'Adv': '/*error!*/',
-		'fn': `var V= oRGB*(1-ic) + U*ic;
-let _X= 0.0266813*V -6,
-_PN= _X<0? -1: 1;
-var brt= _PN*0.473474*Math.pow(Math.abs(_X), 1/7)+ 2.2,
-//Math.pow of fraction can't have negative base
-vrt=V<= 249? 0: V<253.5? (V-249)/15: V<=254.5? .3: -(((V-254.5)/10)+.3);
-con=sat= 1;
-/* 
-can exclude any parameter 
-ex(brt)
-*/
-setFilter(brt, vrt, con, sat);`
-	}
-	items.PozOn= true;
-	items.PozSkip= items.PozSkip= items.AdvOn= false;
-	with(items){
-		PozCSS.apply= '#Brt-opt.Brt-Fixed';
-		PozCSS.position= 'fixed';
-		PozCSS.TB= ['top', ':', 5, 'px'];
-		PozCSS.RL= ['right', ':', 5, 'px'];
-		PozCSS.PT= PozCSS.PR= PozCSS.PB= PozCSS.PL= [5, 'px'];
-		PozCSS.Rad= items.PozCSS.WH= [20, 'px'];
-		PozCSS.Bc= '66ffff';
-	}
-	chrome.storage.sync.set(items);
-	chrome.storage.local.set({"Active":true, "Auto": true});
 }
--- Extension/controll.js
+++ Extension/controll.js
@@ -26,10 +26,9 @@ chrome.storage.sync.get('fn', items=>{
 			}
 			else{
 				items.fn= undefined;
-				var warn= '[BREACH DETECTED]Custom function is invalid, bypased first checheck';
-				chrome.storage.local.set({'Err': {'time':Date(), 'code':407, 'text':warn}});
+				chrome.storage.local.set({'Err': {'time':Date(), 'code':407, 'text':lang.warn.breach}});
 				alert(warn+ ':\n'+ rez);
-				throw new SyntaxError(`%c${warn}:\n`+ rez, 'font-weight:bold');
+				throw new SyntaxError(`%c${lang.warn.breach}:\n`+ rez, 'font-weight:bold');
 			}
 		}
 	}catch(e){
@@ -46,12 +45,12 @@ function Intlize(items){
 	switch(typeof items.Active){
 		case 'undefined':
 		case 'null':
-			throw console.warn('Active is not defined, forced to true');
+			throw console.warn(lang.und.Active);
 			chrome.storage.local.set({
 				'Err':{
 					'time':Date(),
 					'code':404,
-					'text':'Active und, overiden'
+					'text':lang.und.Active
 				},
 				'Active': true
 			});
@@ -76,7 +75,7 @@ function Int2(items){
 		//Start?
 		chrome.storage.local.get(['Active', 'Auto'], items=>{
 			if(items.Active!==false && items.Auto!==false){
-				console.log('Starting');
+				console.log(lang.stat.starting);
 				START();
 			}
 		}
@@ -141,7 +140,7 @@ ${apply}{
 					}
 				}
 			}
-			else console.info('[OFF] Inline IO, User specification')
+			else console.info(lang.stat.inIO)
 		});
 }
 
@@ -166,188 +165,9 @@ function setPl(){
 	}
 	return PLY;
 }
-
-//Active?
-function onPlay(){
-	clearInterval(clock);
-	clock= setInterval(evalu, DLY);
-	toggle();
-}
-
-function onPause(){
-	clearInterval(clock);//Sometimes fails
-	toggle(false);
-}
-
-function StorageChange(changes){
-	try{
-		with(changes.Active){
-			if(newValue== true) START();
-			else if(newValue== false) STOP();
-			else if(newValue== 'Short'){
-				console.info('[STOP] User specification');
-				SHORT();
-			}
-		}
-	}catch(e){}
-}
-//End Active?
-//STP
-function SHORT(){
-	onPause();
-	with(document.getElementsByTagName('video')[0]){
-		style.willChange= 'auto';
-		removeEventListener('play', onPlay);
-		removeEventListener('pause', onPause);
-		removeChild(document.getElementById('Brt-canvas'));
-		removeChild(document.getElementById('Brt-YT'));
-		style.willChange= '';
-	}
-	chrome.storage.onChanged.removeListener(StorageChange);
-}
-function STOP(){
-	onPause();
-	with(document.getElementsByTagName('video')[0]){
-		style.willChange= 'auto';
-		removeEventListener('play', onPlay);
-		removeEventListener('pause', onPause);
-	}
-	document.getElementById('Brt-YT').innerHTML= '';
-}
-function START(){
-	document.getElementsByTagName('video')[0].style.willChange= 'filter';
-	clearInterval(clock);
-	clock= setInterval(evalu, DLY);
-	toggle();
-	with(document.getElementsByTagName('video')[0]){
-		addEventListener('play', onPlay);
-		addEventListener('pause', onPause);
-	}
-}
-//End STP
-function evalu(){
-	if(document.getElementsByClassName('audio_only_div')[0]){
-		SHORT();
-		console.info('[STOP] audio_only detected');
-		return;
-	}
-	if(document.getElementsByTagName('video')[0].style.filter!='' || document.getElementsByTagName('video')[0].readyState< 4) return;
-	if(document.webkitHidden || document.hidden){
-		//*
-			clearInterval(clock);
-			setTimeout(()=>{
-				clock= setInterval(evalu, DLY);
-			}, 5000);
-		//*/
-		return;
-	}
-	//security
-	if(isNaN(oldRgb)){
-		console.warn('Varables ilegaly modifyed');
-		clearInterval(clock);
-		oldRgb= 140;
-		clock= setInterval(evalu, DLY);
-		chrome.storage.local.set({
-			'Err':{
-				'time':Date(),
-				'code':100.7,
-				'text':'oldRgb isNaN->140'
-			}
-		});
-	}
-	document.getElementById('Brt-YT').innerHTML= '';
-	var av= getAvColor(),
-	U= (av.r+av.g+av.b)/3,
-	W= 0.2126*av.r+ 0.7152*av.g+ 0.0722*av.b,
-	rgb= 255-(.9*U+.1*W);
-	/*
-	with(console){
-		groupCollapsed('%crgb: %.2f', "color:blue",rgb);
-		info(av);
-		info('Unweighted rgb(U): %.5f', U);
-		info('Weighted rgb(W): %f.5', W);
-		info('Difference (U-W): %f.5', U-W);
-		info('rgb: %f.5', rgb);
-		timeStamp('time');
-		groupEnd();
-	}//*/
-	var IC= 1;
-	const inc= 1;
-	if(rgb< 254.9 && rgb> 20){
-		while(IC< 10){
-			let ic= IC;
-			setTimeout(CSfn(ic, rgb, U, W, r, g, b, oldRgb, oldW, oldU), (DLY*ic)/10);
-			IC+= inc;
-		}
-		oldRgb= rgb;
-		oldW= W;
-		oldU= U;
-	}
-	else if(rgb<= 20) setFilter(1, 1);
-}
-function setFilter(brt=1, vrt=0, con=1, sat=1){
-	brt= brt==1? '': `brightness(${brt}) `;
-	vrt= vrt==0? '': `invert(${vrt}) `;
-	con= con==1? '': `contrast(${con}) `;
-	sat= sat==1? '': `saturate(${sat})`;
-	if(brt+vrt+con+sat=='') document.getElementById('Brt-YT').innerHTML='';
-	else document.getElementById('Brt-YT').innerHTML= `video{\n\tfilter:${brt+ vrt+ con+ sat}\n}`;
-}
-
-function getAvColor(){
-	let VID=document.getElementsByTagName('video')[0];
-	var o= VID.clientWidth<= 550? 0:1,
-	height= document.getElementById('Brt-canvas').height= VID.clientHeight-SUB*2*o,
-	width= document.getElementById('Brt-canvas').width= VID.clientWidth-SUB*2*o;
-	document.getElementById('Brt-canvas').getContext('2d', {willReadFrequently:true}).drawImage(VID, SUB*o, SUB*o, width, height,0,0,width,height);//hardware acceleration
-    data= document.getElementById('Brt-canvas').getContext('2d', {willReadFrequently:true}).getImageData(0,0, width, height);
-	var i= r= g= b= a= C= 0;
-	while(i< data.data.length){
-		r+= data.data[i];
-		g+= data.data[i+1];
-		b+= data.data[i+2];
-		//a+= data.data[i+3];
-		i+= o==0? 4: Math.round(Math.random()*5 +1)*4;
-		C++;
-	}
-	return {'r':r/C, 'g':g/C, 'b':b/C, 'C':C};
-}
 //Indicate
 function toggle(poz= true){
 	if(PLY!= false){
 		poz?PLY.classList.add('active'):PLY.classList.remove('active');
 	}
-}
-
-//reSet
-function reSet(){
-	var items={
-		'PozCSS': {},
-		'Adv': '/*error!*/',
-		'fn': `var V= oRGB*(1-ic) + U*ic;
-let _X= 0.0266813*V -6,
-_PN= _X<0? -1: 1;
-var brt= _PN*0.473474*Math.pow(Math.abs(_X), 1/7)+ 2.2,
-//Math.pow of fraction can't have negative base
-vrt=V<= 249? 0: V<253.5? (V-249)/15: V<=254.5? .3: -(((V-254.5)/10)+.3);
-con=sat= 1;
-/* 
-can exclude any parameter 
-ex(brt)
-*/
-setFilter(brt, vrt, con, sat);`
-	}
-	items.PozOn= true;
-	items.PozSkip= items.PozSkip= items.AdvOn= false;
-	with(items){
-		PozCSS.apply= '#Brt-opt.Brt-Fixed';
-		PozCSS.position= 'fixed';
-		PozCSS.TB= ['top', ':', 5, 'px'];
-		PozCSS.RL= ['right', ':', 5, 'px'];
-		PozCSS.PT= PozCSS.PR= PozCSS.PB= PozCSS.PL= [5, 'px'];
-		PozCSS.Rad= items.PozCSS.WH= [20, 'px'];
-		PozCSS.Bc= '66ffff';
-	}
-	chrome.storage.sync.set(items);
-	chrome.storage.local.set({"Active":true, "Auto": true});
 }
